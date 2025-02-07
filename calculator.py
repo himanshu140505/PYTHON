182452e9a34387f3040fe3021b0035daf0426be1
himanshu140505 <himanshuyadav4596@gmail.com>
2025-02-07 14:47:02 +0530

    PUSHED EDITS

--- calculator.py
+++ calculator.py
@@ -4,3 +4,22 @@ def calicon():
     print("||                 CALCULATOR                 ||")
     owner()
     print("================================================")
+
+def calculator():
+    while True:
+        expression = input("Enter an expression: ")
+        try:
+            print("----------------------------------------")
+            print("             Result = ", eval(expression))
+            print("----------------------------------------")
+        except:
+            print("XXXXXX Invalid expression XXXXXX")
+        print()
+        choice = input("Do you want to continue? (Y/N)")
+        if choice == 'N' or choice == 'n':
+            print("EXITING.........")
+            break
+
+
+
+calculator()
