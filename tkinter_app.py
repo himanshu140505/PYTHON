8a7147e6a73f68f306cd9918d74ecb7608e975ea
himanshu140505 <himanshuyadav4596@gmail.com>
2025-02-18 22:49:20 +0530

    Added GUI

--- tkinter_app.py
+++ tkinter_app.py
@@ -3,18 +3,23 @@ import tkinter as tk
 
 class TkinterApp:
     def __init__(self, name):
+        self.value = ""
+        self.owner_text = owner_text()
         self.root = tk.Tk()
         self.root.title(name)
-        self.value = ""
-        self.owner_text = owner_text()        
+        
         self.label = tk.Label(self.root, text=f"{self.owner_text}")
         self.label.pack()
+        
         self.text_widget = tk.Text(self.root, height=10, width=50)
         self.text_widget.pack(pady=10)
+
+        self.create_box()
 
     def create_box(self, **kwargs):
         self.input_box = tk.Entry(self.root, **kwargs)
         self.input_box.pack()
+        self.input_box.bind("<Return>", self.on_entry_return)
         return self.input_box
 
     def insert_text(self, text):
@@ -28,7 +33,7 @@ class TkinterApp:
         return self.value
     
     def on_entry_return(self, event):
-        self.insert_text(f"Entry content: {self.input_box.get()}")
+        # self.insert_text(f"Entry content: {self.input_box.get()}")
         print(f"Entry content: {self.input_box.get()}")
         self.input_box.delete(0, tk.END)
     
