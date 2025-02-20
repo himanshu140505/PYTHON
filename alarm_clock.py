dba09bfe539a38b9383c2ff9b1f6bd6c7169b674
himanshu140505 <himanshuyadav4596@gmail.com>
2025-02-20 22:17:47 +0530

    Under Development

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,2 +1,11 @@
+import os
+import sys
 from systemcommands import *
+from tkinter_app import TkinterApp
+
 default()
+
+class ALARM_CLOCK:
+    def aclico(app):
+        TkinterApp.insert_text("")
+
